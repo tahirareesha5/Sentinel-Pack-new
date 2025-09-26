4c70a0458a1ff2615dc82f6cf389c8002ecbb5c6
tahirareesha5 <tahirareesha5@gmail.com>
2025-09-26 16:13:56 +0500

    fix

--- src/extrapage/page.jsx
+++ src/extrapage/page.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import Header from "../components/home/Header";
 import Productbg from "../components/product/Productbg";
-import Categories from "../components/extrapage/categories";
 import Banner from "../components/home/Banner";
 import Footer from "../components/home/Footer";
+import Categories from "../components/extrapage/categories";
 
 function Extrapage() {
   return (
     <>
       <Header />
       <Productbg />
+      {/* <Categories /> */}
       <Categories />
       <Banner />
       <Footer />
